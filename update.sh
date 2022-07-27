c2373ca1134c8d03c6bc0937dfc3317da1de77aa
0x7C2f <76737424+0x7C2f@users.noreply.github.com>
2022-07-27 12:21:18 -0400

    modified:   pages/changelog.md modified:   update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [[ -d "_site" ]]; then
-  neocities push _site
+    neocities push _site
 fi
 git add .
 LANG=C git -c color.status=false status \
@@ -12,6 +12,6 @@ LANG=C git -c color.status=false status \
             -e '/./p' \
 | git commit -F -
 git push -f origin HEAD:gh-pages
-#hub pull-request -h main
+hub pull-request -h main
 
 rake changelog
